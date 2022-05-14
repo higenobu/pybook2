6b87d903547dfc38ba11a2ca40505c43b0a0e2f2
higenobu <33110183+higenobu@users.noreply.github.com>
2022-05-14 15:21:40 -0700

    Add files via upload

---
+++ 10/delimiter.py
@@ -0,0 +1,4 @@
+s = u'今日は晴れていますね．京都に行きませんか．駅で待ち合わせしましょう'
+delimiter ='．'
+v = s.split(delimiter)
+print(v)
---
+++ 10/numberlist.py
@@ -0,0 +1,5 @@
+number_list = [1,2,3,4,5,6,7,8,9]
+for i in range(len(number_list)):
+  print(number_list[i])
+  number_list[i] = number_list[i]*3
+  print(number_list[i])
