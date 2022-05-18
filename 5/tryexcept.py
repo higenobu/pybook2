959dd99eb68e24a2679d617deb0b434e3095a0bb
higenobu <33110183+higenobu@users.noreply.github.com>
2022-05-17 21:03:24 -0700

    Update tryexcept.py

--- 5/tryexcept.py
+++ 5/tryexcept.py
@@ -2,6 +2,7 @@ while(True):
   try:
     str_number = input('整数を入力してください')
     sel_number = int(str_number)
+    sel_number == str_number
     break
   except ValueError:
     print('正しくありません．再度，整数を入力してください')
