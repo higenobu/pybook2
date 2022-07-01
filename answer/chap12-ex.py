3109b210fe5a29457416eb451c0f47a15b4da7d5
higenobu <33110183+higenobu@users.noreply.github.com>
2022-06-30 21:40:26 -0700

    Add files via upload

---
+++ answer/chap12-ex.py
@@ -0,0 +1,31 @@
+import sys
+import csv
+if __name__ == '__main__':
+
+	wks=[]
+	lk=[]
+	datafile='cardealer.csv'
+	wfile='car2022.csv'
+
+	ff=open(datafile,'r')
+	conts=ff.readline()
+	while (conts):
+				
+		print (conts)
+		conts=conts.strip()
+		cc=conts.split(',')
+		print (cc)
+
+		lk.append((cc[0],cc[1],cc[2],cc[3],cc[4],cc[5],cc[6],cc[7]))
+		conts=ff.readline()
+	ff.close()
+	print (lk)
+
+	
+	
+	with open(wfile,'w') as ww:
+		for i in range(len(lk)):
+			print (lk[i])
+			ww.write(lk[i][0]+','+lk[i][1]+','+lk[i][2]+','+lk[i][3]+','+lk[i][4]+','+lk[i][5]+','+lk[i][6]+','+lk[i][7]+'\n')
+              
+        
