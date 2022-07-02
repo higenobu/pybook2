fce2043d3b53558567dbd09ef7d25deda01f6a2d
higenobu <33110183+higenobu@users.noreply.github.com>
2022-07-02 08:15:04 -0700

    Delete 2 directory

--- 2/five.py
+++
@@ -1,5 +0,0 @@
-import myprog 
-instring = 'abcdefg' #文字列の例を代入
-kekka = myprog.substr(instring,0,5)
-#myprogの関数を使って結果を代入
-print(kekka) #結果を出力
--- 2/happytimezero.py
+++
@@ -1,8 +0,0 @@
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-file = open('happy.txt')
-for kurikaeshi in range(1,20):
-  memo = file.readline()
-  print (memo)
-file.close()
