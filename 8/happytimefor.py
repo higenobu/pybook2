e59792f950797e6ca381d77dd1638f8ae85dc6c1
higenobu <33110183+higenobu@users.noreply.github.com>
2022-07-04 15:18:23 -0700

    Delete 8 directory

--- 8/happytimefor.py
+++
@@ -1,7 +0,0 @@
-import time
-file = open('happy.txt')
-for kurikaeshi in range(1,20):
-  memo = file.readline()
-  print(memo)
-  time.sleep(1)
-file.close()
--- 8/happytimelen.py
+++
@@ -1,8 +0,0 @@
-import time
-file = open('happy.txt')
-memo = file.readline()
-while len(memo) > 0:
-  print(memo)
-  time.sleep(1)
-  memo = file.readline()
-file.close()
