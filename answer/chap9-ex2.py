25d3105a69b15347292e77d1d0897c7db8cc6ad5
higenobu <33110183+higenobu@users.noreply.github.com>
2022-07-25 13:45:07 -0700

    Update chap9-ex2.py

--- answer/chap9-ex2.py
+++ answer/chap9-ex2.py
@@ -7,14 +7,7 @@ if __name__ == '__main__':
 	with open(datafile,'r') as ff:
 		reader=csv.reader(ff,delimiter=',')
 		lk=[row for row in reader]
-	'''
-	for dd in range(50):
-		diff=float(lk[dd][1])-float(lk[dd][4])
-		jdate=lk[dd][0]
-		print (diff)
-		diffs.append([jdate,diff])
-	print (diffs)
-	'''
+	
 
 	for dd in range(50):
 		diff=abs(float(lk[dd][2])-float(lk[dd][3]))
