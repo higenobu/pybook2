c7b3f8c9b7a6da021ecc969c0f5b54d999a51037
higenobu <33110183+higenobu@users.noreply.github.com>
2022-08-21 15:48:17 -0700

    Update chap9-ex2.py

--- answer/chap9-ex2.py
+++ answer/chap9-ex2.py
@@ -1,18 +1,18 @@
 import sys
 import csv
-if __name__ == '__main__':
 
-	diffs=[]
-	datafile='nikkei-data.csv'
-	with open(datafile,'r') as ff:
-		reader=csv.reader(ff,delimiter=',')
-		lk=[row for row in reader]
+
+diffs=[]
+datafile='nikkei-data.csv'
+with open(datafile,'r') as ff:
+	reader=csv.reader(ff,delimiter=',')
+	lk=[row for row in reader]
 	
 
-	for dd in range(50):
+for dd in range(50):
 		#high:3,low:4
-		diff=abs(float(lk[dd][3])-float(lk[dd][4]))
-		jdate=lk[dd][0]
-		print (diff)
-		diffs.append([jdate,diff])
-	print (diffs)	
+	diff=abs(float(lk[dd][3])-float(lk[dd][4]))
+	jdate=lk[dd][0]
+	print (diff)
+	diffs.append([jdate,diff])
+print (diffs)	
