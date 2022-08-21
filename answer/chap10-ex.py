5d438fd1be8df313d73f2dcabaf22bafd3cf7445
higenobu <33110183+higenobu@users.noreply.github.com>
2022-08-21 16:35:47 -0700

    Add files via upload

--- answer/chap10-ex.py
+++ answer/chap10-ex.py
@@ -1,32 +1,29 @@
 import sys
 import csv
-if __name__ == '__main__':
 
-	wks=[]
-	lk=[]
-	datafile='work07.csv'
-	wfile='calc07.csv'
-	with open(datafile,'r') as ff:
-		reader=csv.reader(ff,delimiter=',')
-		lk=[row for row in reader]
+wks=[]
+lk=[]
+datafile='work07.csv'
+wfile='calc07.csv'
+with open(datafile,'r') as ff:
+	reader=csv.reader(ff,delimiter=',')
+	for row in reader:
+		lk.append(row)
 	
-	print (lk)
-	for dd in range(len(lk)):
-		wka=str(float(lk[dd][1])*float(lk[dd][4]))
-		wkb=str(float(lk[dd][2])*float(lk[dd][5]))
-		wkc=str(float(lk[dd][3])*float(lk[dd][6]))
-		jdate=lk[dd][0]
+	
+print (lk)
+for dd in range(len(lk)):
+	wka=str(float(lk[dd][1])*float(lk[dd][4]))
+	wkb=str(float(lk[dd][2])*float(lk[dd][5]))
+	wkc=str(float(lk[dd][3])*float(lk[dd][6]))
+	jdate=lk[dd][0]
 		
-		wks.append([jdate,wka,wkb,wkc])
+	wks.append([jdate,wka,wkb,wkc])
 
-	print (wks)	
-	'''
-	with open(wfile,'w') as ww:
-		writer=csv.writer(ww,delimiter=',')
-		writer.writerows(wks)
-	'''
-	with open(wfile,'w') as ww:
-		for i in range(len(wks)):
-			ww.write(wks[i][0]+','+wks[i][1]+','+wks[i][2]+','+wks[i][3]+'\n')
+print (wks)	
+
+with open(wfile,'w') as ww:
+	for i in range(len(wks)):
+		ww.write(wks[i][0]+','+wks[i][1]+','+wks[i][2]+','+wks[i][3]+'\n')
               
           
